2a91141f13b9340aebeb9638f796ed4fc7247d7f
luo-h-x <1877430627@qq.com>
2020-05-21 20:15:16 +0800

    first commit

---
+++ src/pages/test/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="content">
+		<view class="v">q</view>
+		<view class="u">w</view>
+		<view class="e">e</view>
+	</view>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss">
+.content {
+	.v {
+		background: pink;
+		color: $uni-color-success;
+	}
+}
+
+</style>
